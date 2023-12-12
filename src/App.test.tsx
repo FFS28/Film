e0beac76cbacbe227cc904bd164f542dcb54dab0
puck28 <justinnickel28@gmail.com>
2023-12-12 12:34:08 -0500

    update test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/This is test/i);
+  const linkElement = screen.getByText(/This is test1/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          This is test
+          This is test1
         </a>
       </header>
     </div>
