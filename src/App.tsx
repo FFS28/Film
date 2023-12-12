4a53660c53348eba33a939bf1c69be2780dffd19
puck28 <justinnickel28@gmail.com>
2023-12-12 16:16:43 -0500

    add button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <p>
           Hahaha I understood ssh key and gpg key.
         </p>
+        <Button />
         <a
           className="App-link"
           href="https://reactjs.org"
