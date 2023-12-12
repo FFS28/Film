8f9d2056396617d75544eddb46f6b98a2e12a728
puck28 <justinnickel28@gmail.com>
2023-12-12 13:20:00 -0500

    update understood

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hahaha I understood ssh key and gpg key.
         </p>
         <a
           className="App-link"
